dd085fba878739efff601f0229fdee7fdea29e13
mcqueen7 <83740852+mcqueen7@users.noreply.github.com>
2021-10-06 10:34:20 +0800

    Update HW1-Photoresistor.ino

--- HW1-Photoresistor.ino
+++ HW1-Photoresistor.ino
@@ -1,6 +1,6 @@
 void setup() {
   Serial.begin(115200); //設定傳輸鮑率，跟電腦傳輸資料的頻率
-  pinMode(14, INPUT); //設定腳位為輸入用
+  pinMode(13, INPUT); //設定腳位為輸入用
   ledcSetup(0, 5000, 8);   //頻率5000，頻道0，解析度8;
   ledcAttachPin(14, 0);  //將上述頻道0附加到ledPin腳位14上
 }
